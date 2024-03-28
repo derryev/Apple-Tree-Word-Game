d2f8185de17c265c9939e3f01eb3c124775a6b71
derryev <derryev@ssfs.org>
2024-03-28 11:22:42 -0400

    apple tree v2 (working opening screen/buttons)

---
+++ Apple Tree Word Game/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  Apple Tree Word Game
+//
+//  Created by Eva Derryberry on 3/28/24.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
+
+
--- Apple Tree Word Game/ViewController.swift
+++ Apple Tree Word Game/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var listOfWords = ["party", "cat", "garfield", "python", "sparrow", "elephant", "sunflower", "watermelon", "brownie", "homework", "laptop", "apple"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    
+    
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -18,9 +24,20 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
 
-
+    var currentGame: Game!
+    
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
 }
 
